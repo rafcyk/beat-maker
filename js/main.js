7676bcc1cc8855a8fc718a824a9c08b7bc5d146d
Rafcyk <rafcy9@gmail.com>
2021-01-13 23:23:24 +0100

    resize event

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(canvas.width/2, canvas.height/2, 70, 0, 2 * Math.PI);
-ctx.fillStyle = 'black';
-ctx.fill();
+window.addEventListener('resize', resizeWindow)
+
+function resizeWindow() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    draw();
+}
+resizeWindow();
+
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(canvas.width/2, canvas.height/2, 50, 0, 2 * Math.PI)
+    ctx.fillStyle = 'black';
+    ctx.fill();
+}
