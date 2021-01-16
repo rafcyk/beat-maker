5ac89f01586acafb84f4f5d2751ad8340ff4ca43
Rafcyk <rafcy9@gmail.com>
2021-01-16 22:19:14 +0100

    animate js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,31 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext("2d");
+const wrapper = document.querySelector('.wrapper');
+let radius = 0;
+const rects = []
 
-let mainCircleSize = 100;
-let biggerCircle = true;
-
-
-window.addEventListener('resize', resizeWindow)
-
-function resizeWindow() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const centerX = canvas.width/2;
-    const centerY = canvas.height/2;
-    ctx.translate(centerX , centerY);
+for(let i = 0; i < 36; i ++){
+    const rect = document.createElement('div');
+    rect.className = 'eq-rect';
+    rect.style.transform = `rotate(${radius}deg)`;
+    wrapper.appendChild(rect);
+    radius += 10;
+    console.log(radius)
+    rects.push(rect);
 }
-resizeWindow();
 
-canvas.addEventListener('click', drawFigure);
+wrapper.addEventListener('click', showRect);
 
+function showRect() {
 
-
-function drawFigure() {
-    let i = 0;
-    ctx.clearRect(-Math.floor(canvas.width/2),-Math.floor(canvas.height/2), canvas.width, canvas.height)
-
-    const idFirstInterval = setInterval(() => {
+    rects.forEach(rect => {
         let min = Math.ceil(130);
-        let max = Math.floor(150);
-        const finalHeight = Math.floor(Math.random() * (max - min)) + min;
-        ctx.beginPath();
-        ctx.fillStyle = '#2c9da5';
-        ctx.rotate(360/20);
-        i++;
-        
-        const drawRect = setInterval(() => {
-            ctx.fillRect(0,0, 20, min++);
-
-            if(min > finalHeight){
-                clearInterval(drawRect);
-            }
-        }, 10);
-
-        if(i > 36){
-            clearInterval(idFirstInterval);
-        }
-
-        console.log(i);
-    }, 1000/120);
-
+        let max = Math.floor(160);
+        const height = Math.floor(Math.random() * (max - min)) + min;
+        rect.animate([
+            {minHeight: '110px'},
+            {minHeight: `${height}px`},
+            {minHeight: '110px'}
+        ],{
+            duration:400
+        })
+    });
 }
